ba948c67b1825e5a75ac6de36d2e906cbb2ced8c
Aaron <aaron.gerkens@proton.me>
2025-09-17 09:06:46 +0200

    feat: add main layout components

--- index.js
+++ index.js
@@ -27,10 +27,108 @@ const titleBox = blessed.box({
   }
 });
 
+const mainLayout = blessed.layout({
+    parent: screen,
+    top: 5,
+    left: 'center',
+    width: '95%',
+    height: '80%',
+});
+
+const urlInput = blessed.textbox({
+    parent: mainLayout,
+    name: 'urlInput',
+    top: 0,
+    left: 0,
+    height: 3,
+    width: '100%',
+    keys: true,
+    mouse: true,
+    inputOnFocus: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        border: {
+            fg: 'cyan'
+        },
+        focus: {
+            border: {
+                fg: 'lime'
+            }
+        }
+    }
+});
+
+const logBox = blessed.log({
+    parent: mainLayout,
+    top: 4,
+    left: 0,
+    width: '100%',
+    height: '100%-10',
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        border: {
+            fg: 'cyan'
+        }
+    },
+    label: 'Log'
+});
+
+const menu = blessed.list({
+    parent: mainLayout,
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    height: 3,
+    items: ['Download', 'Settings', 'Quit'],
+    keys: true,
+    mouse: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        border: {
+            fg: 'cyan'
+        },
+        selected: {
+            bg: 'blue'
+        }
+    }
+});
+
+// Focus management
+const components = [urlInput, menu];
+let focusIndex = 0;
+
+const setFocus = () => {
+    components[focusIndex].focus();
+    screen.render();
+};
+
+screen.key(['tab'], () => {
+    focusIndex = (focusIndex + 1) % components.length;
+    setFocus();
+});
+
+screen.key(['S-tab'], () => {
+    focusIndex = (focusIndex - 1 + components.length) % components.length;
+    setFocus();
+});
+
+
 // Quit on Escape, q, or Control-C.
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
 });
+
+// Initial focus
+setFocus();
 
 // Render the screen.
 screen.render();
